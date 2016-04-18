52c0d4751312de64ae63e76faa88afcc9ca5cf2d
Kevin Murphy <kevin@retroneogames.com>
2016-04-18 01:18:44 +0100

    New PDN in progress, new script
    Script = Sprite Whitener
    PDN = CAG quarters

---
+++ Assets/Scripts/SpriteWhitener.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteWhitener : MonoBehaviour {
+
+	[Tooltip("If true, the hotkey will toggle the sprite whitener on/off")]
+	public bool debugMode = false;
+	public KeyCode hotkey;
+
+	private SpriteRenderer myRenderer;
+	private Shader shaderGUItext;
+	private Shader shaderSpritesDefault;
+	private Color originalColor;
+
+
+	void Start () 
+	{
+		myRenderer = gameObject.GetComponent<SpriteRenderer>();
+		shaderGUItext = Shader.Find("GUI/Text Shader");
+		shaderSpritesDefault = Shader.Find("Sprites/Default"); // or whatever sprite shader is being used
+		originalColor = myRenderer.color;
+	}
+
+	void Update()
+	{
+		if(debugMode && Input.GetKeyDown(hotkey))
+		{
+			if(myRenderer.material.shader == shaderSpritesDefault)
+				WhitenSprite();
+			else
+				NormalizeSprite();
+		}
+	}
+
+	//To set the sprite to white:
+	public void WhitenSprite() 
+	{
+		myRenderer.material.shader = shaderGUItext;
+		myRenderer.color = Color.white;
+	}
+
+	//And then to set the sprite back to normal:
+	public void NormalizeSprite() 
+	{
+		myRenderer.material.shader = shaderSpritesDefault;
+		myRenderer.color = originalColor;
+	}
+}
