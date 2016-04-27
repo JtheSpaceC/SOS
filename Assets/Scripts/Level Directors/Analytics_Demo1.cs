4a80a729c8587147630addc438aefe59100f269e
Kevin Murphy <kevin@retroneogames.com>
2016-04-27 15:05:15 +0100

    Marcus Work + analytics
    Marcus quartermaster png & temp Stormwall model.
    Moved files around.
    Added (not used yet) Analytics script + Synced analytics

---
+++ Assets/Scripts/Level Directors/Analytics_Demo1.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Analytics;
+using System.Collections;
+
+public class Analytics_Demo1 : MonoBehaviour {
+
+	void OnEnable()
+	{
+//		_battleEventManager.wingmanFirstClash += WingmenDeclareFirstClash;
+//		_battleEventManager.wingmanGotAKill += WingmanDeclareGotAKill;
+//		_battleEventManager.wingmanInTrouble += WingmanDeclareInTrouble;
+//		_battleEventManager.wingmanBack += WingmanDeclareBack;
+//		_battleEventManager.wingmanAcknowledgeOrder += WingmanAcknowledgeOrder;
+//		_battleEventManager.wingmanDied += WingmanDied;
+//
+//		_battleEventManager.pickupOnTheWay += PickupOnTheWay;
+//		_battleEventManager.pickupNegative += PickupNegative;
+//
+//		_battleEventManager.playerLeaving += PlayerLeaving;
+//		_battleEventManager.playerShotDown += PlayerShotDown;
+	}
+	void OnDisable()
+	{
+//		_battleEventManager.wingmanFirstClash -= WingmenDeclareFirstClash;		
+//		_battleEventManager.wingmanGotAKill -= WingmanDeclareGotAKill;
+//		_battleEventManager.wingmanInTrouble -= WingmanDeclareInTrouble;
+//		_battleEventManager.wingmanBack -= WingmanDeclareBack;
+//		_battleEventManager.wingmanAcknowledgeOrder -= WingmanAcknowledgeOrder;
+//		_battleEventManager.wingmanDied -= WingmanDied;
+//
+//		_battleEventManager.pickupOnTheWay -= PickupOnTheWay;
+//		_battleEventManager.pickupNegative -= PickupNegative;
+//
+//		_battleEventManager.playerLeaving -= PlayerLeaving;
+//		_battleEventManager.playerShotDown -= PlayerShotDown;
+	}
+}
