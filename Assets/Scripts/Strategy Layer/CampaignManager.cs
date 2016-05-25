7615744937bdae2c018cf229ea69645230cc1792
Kevin Murphy <kevin@retroneogames.com>
2016-05-25 16:40:13 +0100

    CAG sectors work + new Font (Nasalization)
    New font. Changed all game text to it.
    Moved font folder
    Deleted Bridge PNGS (not in use)
    Work on Sector scripts

---
+++ Assets/Scripts/Strategy Layer/CampaignManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CampaignManager : MonoBehaviour {
+
+	public GameObject sectorsContainer;
+	 public Sector[] allSectors;
+	
+	void Awake()
+	{
+		allSectors = sectorsContainer.GetComponentsInChildren<Sector>();
+	}
+}
+
---
+++ Assets/Scripts/Strategy Layer/Sector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sector:MonoBehaviour{
+
+	SpriteRenderer myRenderer;
+
+	public enum MySectorGroup {Capital, Alpha, Beta, Gamma, Delta};
+	public MySectorGroup mySectorGroup;
+
+	public enum BaseType {None, Civilian, Enemy};
+	public BaseType baseType;
+
+	public bool enemyFleetPresent = false;
+	public int dayExplored = -1;
+
+	void Awake()
+	{
+		myRenderer = GetComponent<SpriteRenderer>();
+
+		if(baseType == BaseType.Civilian)
+			myRenderer.color = Color.blue;
+		else if(baseType == BaseType.Enemy)
+			myRenderer.color = Color.red;
+	}
+
+	public string OutPutMyStats()
+	{
+		return "";	
+	}
+}
