574241456d04cb23f0632f274259d0dd549d0764
Kevin Murphy <kevin@retroneogames.com>
2016-08-18 23:44:15 +0100

    Tried a sprite blending script.
    Failure. Moved script to Unused folder.

---
+++ Assets/Scripts/Unused/SpriteBlendingAnimationTest.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteBlendingAnimationTest : MonoBehaviour {
+
+	public Sprite[] frames;
+
+	public SpriteRenderer rendA;
+	public SpriteRenderer rendB;
+
+	public float fps = 12;
+
+	int currentFrameA;
+	int currentFrameB;
+
+
+	void Start()
+	{
+		rendA.sprite = frames[0];
+		currentFrameA = 0;
+		rendB.sprite = frames[1];
+		currentFrameB = 1;
+
+		StartCoroutine(SpriteBlendAtoB());
+	}
+
+
+	IEnumerator SpriteBlendAtoB()
+	{
+		while(rendA.color.a > 0)
+		{
+			Color newColourA = rendA.color;
+			newColourA.a -= 2f/fps;
+			rendA.color = newColourA;
+
+			Color newColourB = rendB.color;
+			newColourB.a += 2f/fps;
+			rendB.color = newColourB;
+
+			yield return new WaitForEndOfFrame();
+			print(rendA.color.a);
+		}
+
+		currentFrameA += 2;
+
+		if(currentFrameA >= frames.Length)
+			currentFrameA = 0;
+		rendA.sprite = frames[currentFrameA];
+
+		StartCoroutine(SpriteBlendBtoA());
+	}
+
+	IEnumerator SpriteBlendBtoA()
+	{
+		while(rendB.color.a > 0)
+		{
+			Color newColourA = rendA.color;
+			newColourA.a += 2f/fps;
+			rendA.color = newColourA;
+
+			Color newColourB = rendB.color;
+			newColourB.a -= 2f/fps;
+			rendB.color = newColourB;
+
+			yield return new WaitForEndOfFrame();
+		}
+
+		currentFrameB += 2;
+
+		if(currentFrameB >= frames.Length)
+			currentFrameB = 0;
+		rendB.sprite = frames[currentFrameB];
+
+		StartCoroutine(SpriteBlendAtoB());
+	}
+}
