b8ca9f8d97ce6e984749912decc6d05fef7bd111
Kevin Murphy <kevin@retroneogames.com>
2016-09-12 15:30:06 +0100

    Art import, Pilot Names Inspector
    Imported Fred's Art.
    Deleted a concept or two from Laurel.
    Played around with a custom inspector for Names. No great success or
    change.

---
+++ Assets/Editor/CustomPilotNamesInspector.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(Names))]
+public class CustomPilotNamesInspector : Editor {
+
+	Names myNames;
+
+	void OnEnable()
+	{
+		myNames = (Names)target;
+	}
+
+	public override void OnInspectorGUI()
+	{
+	/*	string[] options = new string[]{"Male Names", "Female Names", "Last Names", "Callsigns"};
+
+		if(GUILayout.Button("Male Names"))
+			ShowList(myNames.maleNames.ToArray());
+		if(GUILayout.Button("Female Names"))
+			ShowList(myNames.femaleNames.ToArray());
+		if(GUILayout.Button("Last Names"))
+			ShowList(myNames.lastNames.ToArray());
+		if(GUILayout.Button("Callsigns"))
+			ShowList(myNames.callsigns.ToArray());	
+
+		EditorGUILayout.Popup("Test" ,0, options);*/
+
+		DrawDefaultInspector();
+	}
+
+	void ShowList(string[] namesList)
+	{
+		for(int i = 0; i < namesList.Length; i++)
+		{
+			namesList[i] = GUILayout.TextField(namesList[i]);
+		}
+		base.OnInspectorGUI();
+	}
+}
