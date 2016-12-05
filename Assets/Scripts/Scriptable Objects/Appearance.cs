caf9140375238da74d4127a1078f740511fd3970
Kevin Murphy <kevin@retroneogames.com>
2016-12-05 16:10:02 +0000

    Appearances, Main Menu
    Fixed main menu showing wrong character screen after clicking into
    Character Pool.
    Duplicated Appearances and changed references in new script.
    Old avatar system still works, but ready to be replaced.

--- Assets/Scripts/Scriptable Objects/Appearance.cs
+++ Assets/Scripts/Scriptable Objects/Appearance.cs
@@ -27,5 +27,36 @@ public class Appearance : ScriptableObject {
 	public Color[] spaceSuitColours; 
 	public Sprite[] unitNumbers;
 
+	/*public Sprite[] baseBody;
+	public Sprite[] heads;
+	public Color[] skinTones;
+	public Sprite [] noses;
+	public Sprite[] mouths;
+	public Sprite[] speakingMouthShapes;
+	public Sprite[] shoutingMouthShapes;
+	public Sprite[] tenseMouthSahpes;
+	public Sprite[] eyeWhitesMale;
+	public Sprite[] eyeLidsMale;
+	public Sprite[] eyeIrisesMale;
+	public Sprite[] eyeBlinksMale;
+	public Sprite[] eyeWhitesFemale;
+	public Sprite[] eyeLidsFemale;
+	public Sprite[] eyeIrisesFemale;
+	public Sprite[] eyeBlinksFemale;
+	public Sprite[] facialHair;
+	public Sprite[] hairMale;
+	public Sprite[] hairFemale;
+	public Color[] hairColours;
+	public Sprite[] eyesProp;
+	public Sprite[] scars1;
+	public Sprite[] scares2;
+	public Sprite[] facialFeatures1; 
+	public Sprite[] facialFeatures2; 
+	public Sprite[] clothes;
+	public Sprite[] helmets;
+	public Sprite[] spaceSuits;
+	public Color[] spaceSuitColours; 
+	public Sprite[] unitNumbers;*/
+
 	[HideInInspector] public int avatarWorldPositionModifier = 0; //used at start so avatars aren't on top of each other
 }
